716d1c52b8d9376fc67b2dfa0b5c3b3e7f717d98
孔涛涛 <2909607223@qq.com>
2024-10-17 19:17:17 +0800

    标题和Home页内容

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to 天天出行"/>
+    <HelloWorld msg="Welcome to 10086"/>
   </div>
 </template>
 
