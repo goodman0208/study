bf868a8a9daefc3839c0de5f16c83c5278d5593d
孔涛涛 <2909607223@qq.com>
2024-10-17 18:18:40 +0800

    1017commit2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+const name = process.env.VUE_APP_TITLE || '天天出行H5' // 网页标题
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  lintOnSave: false,// 是否开启eslint保存检测 ,它的有效值为 true || false || 'error'
+  configureWebpack: {
+      name: name,
+      resolve: {
+          alias: {
+              '@': resolve('src')
+          }
+      }
+  },
 })
