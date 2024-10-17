0c2c99d627637d25b1c696ee3c7655cadfdb8a92
孔涛涛 <2909607223@qq.com>
2024-10-17 20:13:23 +0800

    网页标题修改

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
-const name = process.env.VUE_APP_TITLE || '天天出行H5' // 网页标题
+const name = process.env.VUE_APP_TITLE || '中国高等教育学生信息网（学信网）' // 网页标题
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
